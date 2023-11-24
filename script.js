e077f2bf2124dbc0e9188b5c68933e6723a9d9f7
ArthurVS05 <arthur.vincentsil@gmail.com>
2023-11-24 17:22:29 +0100

    ajout slider A and finition menu burger

--- script.js
+++ script.js
@@ -26,9 +26,58 @@ const burger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 burger.addEventListener("click", () => {
+    burger.classList.toggle('active');
     navMenu.classList.toggle("active");
-
-
-
+    document.body.classList.toggle("active");
 }
-)
+);
+
+document.querySelectorAll(".nav-item").forEach(link => {
+    link.addEventListener('click', () => {
+        burger.classList.remove('active');
+        navMenu.classList.remove('active');
+    });
+});
+
+// slider photo A
+// source Internet 
+// https://nouvelle-techno.fr/articles/creer-un-diaporama-en-html-css-js
+
+const sliderA = ["img/nagoya_iwakuni.jpg", "img/tokyo_tana.jpg", "img/mont-fuji-japon.jpg"];
+
+const buttonLeft = document.querySelector(".buttonLeft");
+const buttonRight = document.querySelector(".buttonRight");
+const select_sliderA = document.querySelector("#sliderA");
+
+let compteur = 0;
+
+buttonLeft.addEventListener("click", slideLeft);
+buttonRight.addEventListener("click", slideRight);
+
+// Défiler le diapo vers la gauche
+function slideLeft() {
+    // On décrémente le compteur
+    compteur--;
+    // Si on dépasse le début du diapo, on arrive à la fin
+    if (compteur < 0) {
+        compteur = sliderA.length - 1;
+    }
+    select_sliderA.src = sliderA[compteur];
+
+};
+
+function slideRight() {
+    // On incrémente le compteur
+    compteur++;
+    // Si on arrive à la fin du diapo, on reprend au début
+    if (compteur == sliderA.length) {
+        compteur = 0;
+    }
+    select_sliderA.src = sliderA[compteur];
+
+};
+
+
+
+
+
