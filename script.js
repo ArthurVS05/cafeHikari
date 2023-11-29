f74b3065be6a272ce013dc0d553369e6a1b3164e
ArthurVS05 <arthur.vincentsil@gmail.com>
2023-11-29 12:39:03 +0100

    finalisation version mobile + media query pc

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ document.querySelectorAll(".nav-item").forEach(link => {
     link.addEventListener('click', () => {
         burger.classList.remove('active');
         navMenu.classList.remove('active');
+        document.body.classList.remove('active');
     });
 });
 
@@ -77,7 +78,41 @@ function slideRight() {
 
 };
 
+// slider photo B
 
+const sliderB = ["img/tokyo_tana.jpg", "img/nagoya_iwakuni.jpg", "img/mont-fuji-japon.jpg"];
+
+const buttonLeftB = document.querySelector(".buttonLeftB");
+const buttonRightB = document.querySelector(".buttonRightB");
+const select_sliderB = document.querySelector("#sliderB");
+
+let compteurB = 0;
+
+buttonLeftB.addEventListener("click", slideLeftB);
+buttonRightB.addEventListener("click", slideRightB);
+
+// Défiler le diapo vers la gauche
+function slideLeftB() {
+    // On décrémente le compteur
+    compteurB--;
+    // Si on dépasse le début du diapo, on arrive à la fin
+    if (compteurB < 0) {
+        compteurB = sliderB.length - 1;
+    }
+    select_sliderB.src = sliderB[compteurB];
+
+};
+
+function slideRightB() {
+    // On incrémente le compteur
+    compteurB++;
+    // Si on arrive à la fin du diapo, on reprend au début
+    if (compteurB == sliderB.length) {
+        compteurB = 0;
+    }
+    select_sliderB.src = sliderB[compteurB];
+
+};
 
 
 
